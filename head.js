fcb3f5799e90c6e2f5ca254af0b38c9525f39f8f
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-09-19 14:13:22 -0700

    Created a head function that returns the first element in an array. This code than compares that to the assertEqual function that compares two arguments, (actual and expected).

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}` + "\n");
+
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}` + "\n");
+  }
+};
+
+const head = function(array) {
+  if (array.length === 0) {
+    console.log(`The first element of the array is undefined`);
+    return undefined
+  } else if (array.length > 0) {
+    console.log(`The first element of the array is ${array[0]}`)
+    return array[0];
+  }
+}
+
+
+assertEqual((head([5, 6, 7]), 5), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual((head([25, 50, 75], "testing")), 25.5);
