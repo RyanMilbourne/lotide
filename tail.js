e380afba8c6c240fc7f725c5713fbf0ef42531e1
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-09-21 14:23:03 -0700

    Added return for empty array and array w/ only 1 value

--- tail.js
+++ tail.js
@@ -10,8 +10,12 @@ const assertEqual = function(actual, expected) {
 const tail = function(words) {
   let output = [];
 
-  for (let b = 1; b < words.length; b++) {
-    output.push(words[b]);
+  if (words.length > 1) {
+    for (let i = 1; i < words.length; i++) {
+      output.push(words[i]);
+    }
+  } else if (words.length <= 1) {
+    output = '[]';
   }
   // This console.log prints out the tail values (everything but the head value)
   console.log(`The tail values are ${output}`);
@@ -24,5 +28,6 @@ assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
 const testPass = tail([1, 2, 3, 4, 5]);
 assertEqual(testPass[0], 2);
-const testFail = tail(["Ok", "This", "will", "Fail"]);
-assertEqual(testFail[1], "Supposed to fail");
+
+const testFail = tail(["apple"]);
+assertEqual(testFail, "Supposed to fail");
