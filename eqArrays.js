51effa47f8b7c0b3b20ce6c565b2b5819a3b5d0f
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-09-22 16:30:27 -0700

    Fixed Function to work properly

--- eqArrays.js
+++ eqArrays.js
@@ -6,24 +6,26 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(val1, val2) {
-  let result = '';
-  if (val1.length !== val2.length) {
-    result = false;
+const eqArrays = function(arr1, arr2) {
+  console.log("The values being compared are:", (arr1), "&", (arr2));
+
+  if (arr1.length !== arr2.length) {
+    return false;
   }
 
-  for (let i = 0; i < val1.length; i++) {
-    if (val1[i] === val2[i]) {
-      result = true;
-    } else {
-      result = false;
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
   }
-  console.log("The values being compared are:", (val1), "&", (val2));
-  return result;
+
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3, 4, '5'], [1, 2, 3, 4, 5]), true); // => false)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false)
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["Banana", 1, 2, 3], ["Banana", 1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3, 4, '5'], [1, 2, 3, 4, 5]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["Banana", 1, 2, 3], ["Banana", 1, 2, 3]), true);
+assertEqual(eqArrays(["Kanana", 1, 2, 3], ["Banana", 1, 2, 3]), false);
+assertEqual(eqArrays(["Dog", "Cat", "Bird"], ["Cat", "Rat", "Soup"]), false)
+assertEqual(eqArrays(["frog"], ["stick"]), false);
