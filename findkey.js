ca079ffdf44954b8d48796da42f2531a40418e29
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-09-29 13:21:37 -0700

    Cleaned up

--- findkey.js
+++ findkey.js
@@ -23,7 +23,7 @@ assertEqual(findKey({
   "elBulli": { stars: 3 },
   "Ora": { stars: 2 },
   "Akelarre": { stars: 3 }
-}, x => x.stars === 2), "noma"); // pass
+}, x => x.stars === 2), "noma"); // will pass
 
 assertEqual(findKey({
   "Blue Hill": { stars: 1 },
@@ -32,6 +32,6 @@ assertEqual(findKey({
   "elBulli": { stars: 3 },
   "Ora": { stars: 2 },
   "Akelarre": { stars: 3 }
-}, x => x.stars === 2), "ora"); // fail
+}, x => x.stars === 2), "ora"); // will fail
 
 
