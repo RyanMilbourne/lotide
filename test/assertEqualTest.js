a2ab78dfd4f53c2608e4752708db6ac61bc0dc54
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-11 11:24:42 -0700

    Added ability to test function by implementing require to borrow function from main lotide file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual')
+// Driver Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(100, 100);
+assertEqual(-1, 1);
+assertEqual("Howdy", "Howdy");
+assertEqual("Howdy", "Do Dah");
