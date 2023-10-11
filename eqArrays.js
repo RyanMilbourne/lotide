49728aacd7600e27990e718e9be9cdfa80880044
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-11 11:48:39 -0700

    Added ability to export eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log((`✅✅✅ Assertion Passed: ${actual} === ${expected}`) + "\n");
-  } else {
-    console.log((`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`) + "\n");
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   console.log("The values being compared are:", (arr1), "&", (arr2));
 
@@ -22,10 +14,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3, 4, '5'], [1, 2, 3, 4, 5]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["Banana", 1, 2, 3], ["Banana", 1, 2, 3]), true);
-assertEqual(eqArrays(["Kanana", 1, 2, 3], ["Banana", 1, 2, 3]), false);
-assertEqual(eqArrays(["Dog", "Cat", "Bird"], ["Cat", "Rat", "Soup"]), false)
-assertEqual(eqArrays(["frog"], ["stick"]), false);
+module.exports = eqArrays;
