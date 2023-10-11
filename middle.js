87ba577041579cbc724775b9e10a1766166464f8
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-11 13:15:02 -0700

    imported eqArrays function as well as ability to export middle function

--- middle.js
+++ middle.js
@@ -1,28 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  console.log("The values being compared are:", (arr1), "&", (arr2));
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i <= arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  const result = eqArrays(arr1, arr2);
-  if (result) {
-    console.log("True ✅" + "\n");
-  } else {
-    console.log("False ❌" + "\n");
-  }
-  return result;
-};
+const eqArrays = require('./eqArrays')
 
 const middle = function(array) {
   let output = [];
@@ -41,8 +17,4 @@ const middle = function(array) {
   return output;
 };
 
-const testArr1 = [0, 50, 100];
-assertArraysEqual(middle(testArr1), ["50"]); // Will result in False
-
-const testArr2 = ["hello", "world", "lighthouse"];
-assertArraysEqual(middle(testArr2), ["world"]); // Will result in True
+module.exports = middle;
