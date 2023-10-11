052f8307a123a3abe9d70e05a10c6fccb96dfd0b
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-11 13:51:14 -0700

    Implemented proper framework for Mocha & Chai testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,29 @@
-const assertEqual = require('../assertEqual')
+// const assertEqual = require('../assertEqual')
+// const head = require('../head')
+const assert = require('chai').assert;
 const head = require('../head')
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// Driver Code
-assertEqual((head([5, 6, 7]), 5), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual((head([25, 50, 75], "testing")), 25.5);
-assertEqual((head([5, 6, 7]), 5), 5);
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+  });
+
+  it("returns 25 for ([25, 50, 75], 'testing') which does not equal 25.5", () => {
+    assert.notStrictEqual(head([25, 50, 75], "testing"), 25.5)
+  });
+
+  it("returns 5 for ([5, 6, 7]), 5)", () => {
+    assert.strictEqual(head([5, 6, 7], 5), 5)
+  });
+})
