fdca466163a8206dfe42ab748f123fe09039bed3
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-11 11:38:58 -0700

    Added ability to export tail function

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}` + "\n");
-
-  } else {
-    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}` + "\n");
-  }
-};
-
 const tail = function(words) {
   let output = [];
 
@@ -18,16 +9,8 @@ const tail = function(words) {
     output = '[]';
   }
   // This console.log prints out the tail values (everything but the head value)
-  console.log(`The tail values are ${output}`);
+  console.log(`The tail values are ${output} \n`);
   return output;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-const testPass = tail([1, 2, 3, 4, 5]);
-assertEqual(testPass[0], 2);
-
-const testFail = tail(["apple"]);
-assertEqual(testFail, "Supposed to fail");
+module.exports = tail;
