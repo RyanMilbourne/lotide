ff13c20c10e86fdabc9f02f0d456eea32926bcc9
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-17 17:01:25 -0700

    findKey function now exported and available via the lotide library

--- findKey.js
+++ findKey.js
@@ -1,13 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log((`✅✅✅ Assertion Passed: ${actual} === ${expected}`) + "\n");
-  } else {
-    console.log((`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`) + "\n");
-  }
-};
-
 const findKey = function(object, callback) {
-
   for (const key in object) {
     if ((callback(object[key]))) {
       return key;
@@ -16,22 +7,4 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-}, x => x.stars === 2), "noma"); // will pass
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-}, x => x.stars === 2), "ora"); // will fail
-
-
+module.exports = findKey;
