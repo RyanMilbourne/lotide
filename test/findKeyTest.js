acb05edd3c7010d2c3aba49dad4ae6aa6dbd6ad3
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-17 17:02:19 -0700

    testing file for findKey function

---
+++ test/findKeyTest.js
@@ -0,0 +1,20 @@
+const assertEqual = require('../assertEqual')
+const findKey = require('../findkey')
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma"); // will pass
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "ora"); // will fail
