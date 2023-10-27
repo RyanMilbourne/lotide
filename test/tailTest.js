86925e499b525b3c4ad242ed37fb2c34949776ae
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-27 16:09:21 -0700

    linting applied

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const tail = require('../tail')
+const tail = require('../tail');
 
 describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
@@ -7,14 +7,14 @@ describe("#tail", () => {
   });
 
   it('returns  ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 
   it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
-    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5])
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
   });
 
   it("returns [] for ['apple']", () => {
-    assert.deepEqual(tail(["apple"]), '[]')
+    assert.deepEqual(tail(["apple"]), []);
   });
-})
+});
