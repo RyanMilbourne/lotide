5f762ca5bde58b09e84f9b4a83a240bef58ecb8b
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-27 15:47:54 -0700

    lint applied

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,5 @@
-// const assertEqual = require('../assertEqual')
-// const head = require('../head')
 const assert = require('chai').assert;
-const head = require('../head')
+const head = require('../head');
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
@@ -16,14 +14,14 @@ describe("#head", () => {
   });
 
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   });
 
   it("returns 25 for ([25, 50, 75], 'testing') which does not equal 25.5", () => {
-    assert.notStrictEqual(head([25, 50, 75], "testing"), 25.5)
+    assert.notStrictEqual(head([25, 50, 75], "testing"), 25.5);
   });
 
   it("returns 5 for ([5, 6, 7]), 5)", () => {
-    assert.strictEqual(head([5, 6, 7], 5), 5)
+    assert.strictEqual(head([5, 6, 7], 5), 5);
   });
-})
+});
