4ce6bca44c1246e76286187ff1d305a832174a6b
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-17 10:27:31 -0800

    add general clean-up & export functionality

--- without.js
+++ without.js
@@ -8,34 +8,4 @@ const without = function(source, itemsToRemove) {
   return newValue;
 };
 
-const eqArrays = function(arr1, arr2) {
-  console.log("The values being compared are:", (arr1), "&", (arr2));
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i <= arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  const result = eqArrays(arr1, arr2);
-  if (result) {
-    console.log("True ✅" + "\n");
-  } else {
-    console.log("False ❌" + "\n");
-  }
-  return result;
-};
-
-
-console.log(without([1, 2, 3], [1])); // => [2, 3] should be printed
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"]));
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
