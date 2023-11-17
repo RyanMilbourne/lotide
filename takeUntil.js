6e8dd56389bde423a98dbbfd4bbe85b054699877
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-17 10:25:23 -0800

    add general clean-up and export functionality

--- takeUntil.js
+++ takeUntil.js
@@ -1,17 +1,5 @@
 const takeUntil = function(array, callback) {
 
-  /* my old method
-   // let end = array.length - 1;
-   // for (const x of array) {
-   //   if (callback(x)) {
-   //     end = array.indexOf(x);
-   //   }
-   // }
-   // return array.slice(0, end);
-   // };
-   */
-
-  // refactored with help from mentor
   const finalResult = [];
 
   for (let i = 0; i < array.length; i++) {
@@ -21,21 +9,6 @@ const takeUntil = function(array, callback) {
       return finalResult;
     }
   }
-}
+};
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-// expected results
-/*
-[ 1, 2, 5, 7, 2 ]
---
-[ 'I\'ve', 'been', 'to', 'Hollywood' ]
-*/
+module.exports = takeUntil;
