33a162b7bfc1c97f8a3e9090c70e3a380e1752cd
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-17 10:23:48 -0800

    add linting

--- head.js
+++ head.js
@@ -1,11 +1,11 @@
 const head = function(array) {
   if (array.length === 0) {
     console.log(`The first element of the array is undefined`);
-    return undefined
+    return undefined;
   } else if (array.length !== 0) {
-    console.log(`The first element of the array is ${array[0]}`)
+    console.log(`The first element of the array is ${array[0]}`);
     return array[0];
   }
-}
+};
 
 module.exports = head;
