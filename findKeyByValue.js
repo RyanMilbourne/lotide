0a7af2168c854da4eb5b8e26930043c2a5f93b4b
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-17 10:34:41 -0800

    remove test-runs

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,3 @@
-const assertEquals = require('./assertEqual');
-
 const findKeyByValue = function(obj, val) {
   for (const genre in obj) {
     if (obj[genre] === val) {
@@ -9,17 +7,5 @@ const findKeyByValue = function(obj, val) {
   }
   return;
 };
-
-// test runs
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-assertEquals(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // pass
-assertEquals(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // pass
-assertEquals(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama"); // fail
 
 module.exports = findKeyByValue;
