ae56fe4207c6ce21b9cda90353d001df15cd9d94
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-17 10:10:19 -0800

    add general cleanup & export functionality

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,28 +1,25 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log((`✅✅✅ Assertion Passed: ${actual} === ${expected}`) + "\n");
-  } else {
-    console.log((`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`) + "\n");
-  }
-};
+const assertEquals = require('./assertEqual');
 
 const findKeyByValue = function(obj, val) {
-
   for (const genre in obj) {
     if (obj[genre] === val) {
       console.log(`"${val}" falls under the ${genre} genre.`);
       return genre;
     }
   }
-
   return;
 };
+
+// test runs
 
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire"
 };
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama"); // will fail
+
+assertEquals(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // pass
+assertEquals(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // pass
+assertEquals(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama"); // fail
+
+module.exports = findKeyByValue;
